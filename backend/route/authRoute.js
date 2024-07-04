ad83be3ef7f6fb0c44a9f34e26c200cc921cf3ec
Pushpendra <pushpendraksingh82@gmail.com>
2024-07-04 20:40:46 +0530

    backend-added

---
+++ backend/route/authRoute.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import { registerController ,loginController,forgotPasswordController,testController} from '../controllers/authController.js'
+import { requireSignIn,isAdmin } from '../middlewares/authMiddleware.js';
+//router object
+const router = express.Router();
+
+//routing
+//REGISTER || METHOD POST
+router.post("/register", registerController);
+
+//LOGIN || POST
+router.post("/login", loginController);
+
+//Forgot Password || POST
+router.post("/forgot-password", forgotPasswordController);
+
+//test routes
+router.get("/test", requireSignIn, testController);
+
+//protected route auth
+router.get("/user-auth", requireSignIn, (req, res) => {
+  res.status(200).send({ ok: true });
+});
+
+export default router;
