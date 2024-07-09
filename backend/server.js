2fe90eb487d3c5aba9a85b008dffc3206ebd3037
Prashant Kumar <prashantofficial1304@gmail.com>
2024-07-09 19:44:06 +0530

    added cors policy

--- backend/server.js
+++ backend/server.js
@@ -32,9 +32,21 @@ app.use(morgan('dev'));
 app.use(cookieParser());
 
 // Enable CORS
-app.use(cors({
-    credentials: true, // This allows the browser to include cookies in requests
-  }));
+const allowedOrigins = ['http://localhost:3000', 'https://mega-share.vercel.app']; // List your allowed origins here
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true); // Allow the request
+    } else {
+      callback(new Error('Not allowed by CORS')); // Deny the request
+    }
+  },
+  credentials: true, // Allow cookies to be sent
+};
+
+app.use(cors(corsOptions)); // Apply the CORS middleware
+
   
 
 //routes
